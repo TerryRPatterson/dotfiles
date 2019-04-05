c394fd78216acef63fb3c2cfb5d5c72b0a51628d
Terry Patterson <tpatterson@datatrac.com>
2019-04-05 15:32:46 -0400

    4/5/2019

--- .vscode/extensions/octref.vetur-0.17.1/server/dist/modes/script/javascript.d.ts
+++
@@ -1,6 +0,0 @@
-import { LanguageModelCache } from '../languageModelCache';
-import { LanguageMode } from '../languageModes';
-import { VueDocumentRegions } from '../embeddedSupport';
-import { VueInfoService } from '../../services/vueInfoService';
-import { DependencyService } from '../../services/dependencyService';
-export declare function getJavascriptMode(documentRegions: LanguageModelCache<VueDocumentRegions>, workspacePath: string | undefined, vueInfoService?: VueInfoService, dependencyService?: DependencyService): Promise<LanguageMode>;
--- .vscode/extensions/octref.vetur-0.17.1/server/dist/modes/template/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { LanguageModelCache } from '../languageModelCache';
-import { LanguageMode } from '../languageModes';
-import { VueDocumentRegions } from '../embeddedSupport';
-import { VueInfoService } from '../../services/vueInfoService';
-declare type DocumentRegionCache = LanguageModelCache<VueDocumentRegions>;
-export declare function getVueHTMLMode(documentRegions: DocumentRegionCache, workspacePath: string | undefined, vueInfoService?: VueInfoService): LanguageMode;
-export {};
